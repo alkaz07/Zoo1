02f1c50cd351a0fe8c18fd0418d2ff3fd7e66680
MaxMitin <maksim.mitin@mail.ru>
2023-08-09 12:37:29 +0300

    Corrected description of the method is added.

--- src/Penguin.java
+++ src/Penguin.java
@@ -5,6 +5,9 @@ public class Penguin extends Animal implements Swimmable {
     }
 
     @Override
-    public void move() {System.out.println("Пингвин плавает как показано в добром советском мультфильме 'Приключения пингвинёнка Лоло'.");}
-
+    public void move() {
+        swim();
     }
+    @Override
+    public void swim() {System.out.println("Пингвин плавает как показано в добром советском мультфильме 'Приключения пингвинёнка Лоло'.");}
+}
