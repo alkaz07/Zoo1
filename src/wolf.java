11840f46c9a229d1d2e2217e44cd8a148b606185
AlexSh888 <141142477+AlexSh888@users.noreply.github.com>
2023-08-09 12:44:54 +0300

    Сделан Волк

---
+++ src/wolf.java
@@ -0,0 +1,33 @@
+public class wolf extends Mammal implements Jumpable,Runnable,Swimmable,Sneakable {
+
+    public wolf(double weight) {
+        super(weight);
+    }
+    @Override
+        public void move() {
+            run();
+            swim();
+            sneaking();
+            jump();
+        }
+    private void sneaking() {
+        System.out.println("Волк крадётся");
+    }
+    @Override
+        public void jump() {
+            System.out.println("Волк прыгает");
+        }
+        @Override
+        public void run() {
+            System.out.println("Волк бежит");
+        }
+        @Override
+        public void swim() {
+            System.out.println("Волк плавает");
+                }
+    @Override
+    public void sneak() {
+        System.out.println("Волк крадётся");
+    }
+}
+
